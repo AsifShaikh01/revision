8ea17c04897a5ab4783ba28a49d7fa8ccf8e0983
AsifShaikh01 <asifshaikh0024@gmail.com>
2023-06-13 14:06:05 +0530

    ok

--- routes/Job.routes.js
+++ routes/Job.routes.js
@@ -5,7 +5,7 @@ const jobRouter = express.Router();
 
 jobRouter.get("/",async(req,res)=>{
     const query = req.query;
-    const {sortBy , filterBy} = req.query;
+    // const {sortBy , filterBy} = req.query;
 
    
 
@@ -16,18 +16,9 @@ jobRouter.get("/",async(req,res)=>{
 
 
     try {
-        const sorted = {};
-        if(sortBy === "postedAt"){
-            sorted.postedAt = -1
-        }
         
-    const page = parseInt(req.query.page) || 1;
-    const perPage = 1;
-    const skipCount = (page -1) * perPage;
     const jobs = await JobModel.find(query)
-    .skip(skipCount)
-    .limit(perPage)
-    .sort({postedAt : -1});
+    
     res.send(jobs)
     } catch (error) {
         res.send(error)
